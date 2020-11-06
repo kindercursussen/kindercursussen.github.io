e3bbb2d632f99231168f0d6b4c02cb97b2f40099
knijtram <mgakuipers@gmail.com>
2020-11-06 18:15:38 +0100

    fixed img en vw compadibility

--- js/java.js
+++ js/java.js
@@ -7,13 +7,15 @@ function burger_open_close() {
 
   if (x.className === "navbar_burger") {
     x.className += " responsive";
-    y.className += " invert";
+    y.className += " pos";
+    y.src = 'img/burger_icon_invert.png';
     body.className += " overflowY";
     window.setTimeout(opacityAddTitle, 500);
     window.setTimeout(opacityAddMenu, 1000);
   } else {
     x.className = "navbar_burger";
     y.className = "burger_icon";
+    y.src = 'img/burger_icon.png';
     z.className = "title";
     w.className = "nav_menu_burger";
     body.className = "";
