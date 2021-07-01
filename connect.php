c40366e2d9137fbe331e453d09401642c393b1f6
knijtram <mgakuipers@gmail.com>
2021-07-01 22:56:28 +0200

    Connect bestand weggehaald

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //DataBase Connection
-    $conn = new mysqli('localhost','root','Martijn12', 'db_kindercursussen');
-    if($conn->connect_error) {
-        die('Connection failed: ' .$conn->connect_error);
-    }
-
-?>
